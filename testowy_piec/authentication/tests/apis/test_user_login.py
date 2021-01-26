0bf56d1850daf9b4506b7eeb2eaff857596f0fe4
r2r <aart.st@gmail.com>
2021-01-26 12:57:37 +0100

    fixed auth tests

--- testowy_piec/authentication/tests/apis/test_user_login.py
+++ testowy_piec/authentication/tests/apis/test_user_login.py
@@ -23,25 +23,18 @@ def test_non_existing_user_cannot_login(client: APIClient):
 
     response = client.post(url, data)
 
-    # {'detail': ErrorDetail(string='No active account found with the given credentials', code='no_active_account')}
-
     assert response.status_code == 401
 
 
 @pytest.mark.django_db(transaction=True)
 def test_existing_user_can_login_and_access_apis(client: APIClient, user: BaseUser):
-    assert client.login(username=user.email, password='test')
-    token = Token.objects.create(user=user)
-    client.credentials(HTTP_AUTHORIZATION='Bearer ' + token.key)
+    url = reverse('api:authentication:token_obtain_pair')
+    data = {'email': user.email, 'password': 'test'}
+    response = client.post(url, data)
+    client.credentials(HTTP_AUTHORIZATION='Bearer ' + response.data['access'])
 
     url = reverse('api:authentication:me')
     data = {'email': user.email}
     response = client.get(url, data=data)
 
     assert response.status_code == 200
-    """
-    1. Create user
-    2. Assert login is OK
-    3. Call /api/auth/me
-    4. Assert valid response
-    """
