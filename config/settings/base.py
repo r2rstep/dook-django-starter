abddb510cc736138e83d70a1fa99460a0865ccfa
Artur Stępniak <aart.st@gmail.com>
2021-02-01 09:02:05 +0100

    configure default pagination in django settings add UT to check items pagination

--- config/settings/base.py
+++ config/settings/base.py
@@ -158,5 +158,6 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'testowy_piec.api.pagination.LimitOffsetPagination',
 }
--- testowy_piec/items/tests/test_apis.py
+++ testowy_piec/items/tests/test_apis.py
@@ -1,6 +1,10 @@
+from collections import OrderedDict
+
 from django.urls import reverse
 from rest_framework.test import APIClient
 import pytest
+
+from .factories import ItemFactory
 
 
 @pytest.fixture
@@ -19,3 +23,48 @@ def test_post(client):
     response = client.post(f'{url}', data)
 
     assert response.status_code == 201
+
+
+@pytest.mark.django_db(transaction=True)
+def test_paginated_list(client):
+    items = []
+    for _ in range(0, 3):
+        items.append(ItemFactory())
+
+    url = reverse('api:items:items')
+    expected_first_page_response = OrderedDict({
+        'limit': 1,
+        'offset': 0,
+        'count': len(items),
+        'next': f'http://testserver{url}?limit=1&offset=1',
+        'previous': None,
+        'results': [
+            OrderedDict({
+                'id': items[0].id,
+                'name': items[0].name,
+                'prop': items[0].prop,
+            })
+        ]
+    })
+
+    response = client.get(f'{url}?limit=1')
+    assert response.status_code == 200
+    assert response.data == expected_first_page_response
+
+    response = client.get(response.data['next'])
+    expected_next_page_response = OrderedDict({
+        'limit': 1,
+        'offset': 1,
+        'count': len(items),
+        'next': f'http://testserver{url}?limit=1&offset=2',
+        'previous': f'http://testserver{url}?limit=1',
+        'results': [
+            OrderedDict({
+                'id': items[1].id,
+                'name': items[1].name,
+                'prop': items[1].prop,
+            })
+        ]
+    })
+    assert response.status_code == 200
+    assert response.data == expected_next_page_response
